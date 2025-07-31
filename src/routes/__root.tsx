8d0f56a23f21a34fc6c246b9a83dce3d80fa19b1
Phisict <adrianfathan69@gmail.com>
2025-07-31 19:33:15 +0700

    Fix flashing light

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,13 +9,14 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-
-        <Header />
-        <div className='min-h-screen w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] dark:bg-[radial-gradient(#3f3f46_1px,transparent_1px)]'>
-          <Outlet />
-        </div>
-        <Footer />
-        {/* <TanStackRouterDevtools /> */}
+        <main className='bg-white dark:bg-black'>
+          <Header />
+          <div className='min-h-screen w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] dark:bg-[radial-gradient(#3f3f46_1px,transparent_1px)]'>
+            <Outlet />
+          </div>
+          <Footer />
+          {/* <TanStackRouterDevtools /> */}
+        </main>
       </ThemeProvider>
     </>
   ),
